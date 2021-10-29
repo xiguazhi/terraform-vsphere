f1ed035d2ad2cde3b6fa11ac8fa921865f11a0b2
Brandon Sorenson <sorebran@isu.edu>
2021-10-28 22:17:20 -0600

    change to local backend

--- dev/00-backend.tf
+++ dev/00-backend.tf
@@ -1,9 +1,5 @@
 terraform {
-    backend "s3" {
-        region = "us-west-2"
-        access_key = var.access_key
-        secret_key = var.secret_key
-        bucket = "bs-autocount-upload"
-        key = var.s3_folder
+    backend "local" {
+        path = ~/terraform/terraform.tfstate
     }
 }
