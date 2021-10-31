284f5078c3ec7b09ced58dd208cbccf80e99cfe7
Brandon Sorenson <sorebran@isu.edu>
2021-10-30 23:14:38 -0600

    created custom vm module with more customizations.

---
+++ modules/wtw-vm-manual/main.tf
@@ -0,0 +1,36 @@
+provider "vsphere" {
+  vsphere_server = var.env[var.environment].vcenter_server
+  
+  user           = var.vsphere_user
+  password       = var.vsphere_password                # Nothing will be hardcoded and checked into the repo but in development sops with locall stored key is used for credentials.\
+  
+
+  # if you have a self-signed cert
+  allow_unverified_ssl = true
+
+}
+
+module "wtw-onprem-linux" {
+  source                    = "Terraform-VMWare-Modules/vm/vsphere"
+  version                   = "3.4.0"
+  count                     = var.env[var.environment].instances
+  vmname                    = format("${var.env[var.environment].app_prefix}${var.namefmt}-${var.application_name}", count.index + 1)
+  staticvmname              = format("${var.env[var.environment].app_prefix}${var.namefmt}-${var.application_name}", count.index + 1)
+  dc                        = var.env[var.environment].dc
+  vmrp                      = var.env[var.environment].vmrp #Works with ESXi/Resources
+  vmfolder                  = var.env[var.environment].vmfolder #Works with ESXi/Resources
+  datastore                 = var.env[var.environment].ds_cluster #You can use datastore variable instead
+  vmtemp                    = var.vmtemp
+  instances                 = var.env[var.environment].instances
+  cpu_number                = var.env[var.environment].cpu_number
+  num_cores_per_socket      = var.env[var.environment].num_cores_per_socket
+  memory_hot_add_enabled    = var.env[var.environment].memory_hot_add_enabled
+  ram_size                  = var.env[var.environment].ram_size
+  domain                    = "bsorenson.io"
+  network = {
+    "Server VLAN" = var.ipv4_address # To use DHCP create Empty list ["",""]
+  }
+  dns_server_list  = var.dns_server_list
+  vmgateway        = var.ipv4_gateway
+  network_type    = ["vmxnet3"]
+}
---
+++ modules/wtw-vm-manual/variables.tf
@@ -0,0 +1,188 @@
+variable "DATACENTER" {
+    type = map(object({
+        alias = string
+        vcenter_server = string
+
+    }))
+    default = {
+        "TEXAS" = {
+        alias = "TEXAS",
+        vcenter_server = "bsorenson.vcenter.io"
+        },
+        "UTAH" = {
+        alias = "UTAH",
+        vcenter_server = "bsorenson.vcenter.io"
+        }
+    }
+}
+
+
+
+variable "env" {
+    type = map(object({
+        vcenter_server = string
+        dc = string
+        app_prefix = string
+        vmrp = string
+        vmfolder = string
+        ds_cluster = string
+        instances = number
+        cpu_number = number
+        num_cores_per_socket = number
+        memory_hot_add_enabled = bool
+        ram_size = number
+        ipv4submask = list(string)
+        ipv4network = string
+        vmgateway = string
+        dns_server_list = list(string)
+        alias = string
+    }))
+    default = {
+        dev = {
+            vcenter_server = "10.0.4.41"
+            dc = "bsorenson.io",
+            app_prefix = "bsd",
+            vmrp = "prod",
+            vmfolder = "Pipeline",
+            ds_cluster = "wdBlue",
+            instances = 1,
+            cpu_number = 1,
+            num_cores_per_socket = 4,
+            ram_size = 8192,
+            memory_hot_add_enabled = true,
+            ipv4submask = ["24"],
+            ipv4network = "Server VLAN"
+            vmgateway = "10.0.30.1",
+            dns_server_list = ["10.0.30.63"],
+            alias = "TEXAS"
+        }
+        QA  = {
+            vcenter_server = "10.0.4.41"
+            dc = "TEXAS",
+            app_prefix = "txq",
+            vmrp = "Richardson",
+            vmfolder = "QA/Application",
+            ds_cluster = "wdBlue",           
+            instances = 2,
+            cpu_number = 1,
+            num_cores_per_socket = 4,
+            ram_size = 8192,
+            memory_hot_add_enabled = true,
+            ipv4submask = ["24"],
+            ipv4network = "Server VLAN",
+            vmgateway = "10.0.30.1",
+            dns_server_list = ["10.0.30.63"],
+            alias = "TEXAS"
+
+        },
+       stage = {
+           vcenter_server = "vcenter.bsorenson.io"
+            dc = "UTAH",
+            app_prefix = "wjs",
+            vmrp = "WestJordan",
+            vmfolder = "Stage/Application",
+            ds_cluster = "wdBlue",
+            instances = 3,
+            cpu_number = 1,
+            num_cores_per_socket = 4,
+            ram_size = 8192
+            memory_hot_add_enabled = true,
+            ipv4submask = ["24"],
+            ipv4network = "Server VLAN",
+            vmgateway = "10.0.30.1",
+            dns_server_list = ["10.0.30.63"],
+            alias = "UTAH"
+
+        }
+        prod = {
+            vcenter_server = "vcenter.bsorenson.io"
+            dc = "UTAH",
+            app_prefix = "wjp",
+            vmrp = "WestJordan",
+            vmfolder = "prod/Application",
+            ds_cluster = "wdBlue",
+            instances = 4,
+            cpu_number = 1,
+            num_cores_per_socket = 4,
+            ram_size = 8192,
+            memory_hot_add_enabled = true,
+            ipv4submask = ["24"],
+            ipv4network = "Server VLAN",
+            vmgateway = "10.0.30.1",
+            dns_server_list = ["10.0.30.63"],
+            alias = "UTAH"
+
+        }
+       manual = {
+            vcenter_server = "vcenter.bsorenson.io"
+            dc = "UTAH",
+            app_prefix = "wjp",
+            vmrp = "WestJordan",
+            vmfolder = "prod/Application",
+            ds_cluster = "wdBlue",
+            instances = 4,
+            cpu_number = 1,
+            num_cores_per_socket = 4,
+            ram_size = 8192,
+            memory_hot_add_enabled = true,
+            ipv4submask = ["24"],
+            ipv4network = "Server VLAN",
+            vmgateway = "10.0.30.1",
+            dns_server_list = ["10.0.30.63"],
+            alias = "UTAH"
+        }
+    }
+
+}
+
+variable "application_name" {
+    type = string
+    default = "actions"
+}
+
+variable "ipv4_gateway" {
+    type = string
+    default = "10.0.30.1"
+}
+
+variable ipv4_address {
+    type = list
+    default = [""]
+}
+
+
+variable "namefmt" {
+    type = string
+    default = "%0d"
+}
+
+
+variable "domain" {
+    type = string
+    default = "bsorenson.io"
+}
+  
+
+variable "environment" {
+    type = string
+    description = "Environment type."
+
+    validation {
+      condition     = contains(["dev","QA","stage","prod"], var.environment)
+      error_message = "Valid values for var: environment are (dev, QA, stage, prod)."
+    }
+}
+
+variable "vmtemp" {
+    type = string
+    description = "VM Template."
+}
+
+variable "vsphere_user" {}
+
+variable "vsphere_password" {}
+
+variable "dns_server_list" {
+    type = list(string)
+    default = ["1.1.1.1","1.0.0.1"]
+}
