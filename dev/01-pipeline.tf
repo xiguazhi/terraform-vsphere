6993a88853234f84a8a7d818f7f5b219d17cc0f1
Brandon Sorenson <sorebran@isu.edu>
2022-07-31 17:01:51 -0600

    ttt12

--- dev/01-pipeline.tf
+++ dev/01-pipeline.tf
@@ -10,6 +10,11 @@ module "pipeline" {
   environment      = "dev"
   source           = "../modules/wtw-vm-linux/"
   vmtemp           = "Ubuntu2004"
+  cpu_share_count  = 1000
+  memory           = 8192
+  memory_share_count = 81920
+  num_cores_per_socket = 4
+  num_cpus         = 1
   application_name = "pipeline"
   vsphere_user     = var.vsphere_user
   vsphere_password = var.vsphere_password
